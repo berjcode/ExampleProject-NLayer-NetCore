a48e52fa097d72e327811fcd295a662b29a3dd17
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2023-05-02 12:03:13 +0300

    Role and User Entities are created.

---
+++ ExampleProject.Entities/Concrete/Role.cs
@@ -0,0 +1,10 @@
+﻿using ExampleProject.Core.Entities.Abstract;
+
+namespace ExampleProject.Entities.Concrete;
+
+public sealed class Role : EntityBase, IEntity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public ICollection<User> Users { get; set; }
+}
---
+++ ExampleProject.Entities/Concrete/User.cs
@@ -0,0 +1,15 @@
+﻿
+using ExampleProject.Core.Entities.Abstract;
+
+namespace ExampleProject.Entities.Concrete;
+
+public sealed class User:EntityBase,IEntity
+{
+    public string Name  { get; set; }
+    public string LastName { get; set; }
+    public string UserName { get; set; }
+    public byte[]  PasswordHash { get; set; }
+    public int RoleId { get; set; }
+    public Role  Role{ get; set; }
+
+}
