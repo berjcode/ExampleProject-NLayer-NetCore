986fb87a8d4cfef13f3c4db7770f27090da160d4
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2023-05-02 12:41:27 +0300

    IEntityRepository created.

---
+++ ExampleProject.Core/Data/Abstract/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿
+
+using ExampleProject.Core.Entities.Abstract;
+using System.Linq.Expressions;
+
+namespace ExampleProject.Core.Data.Abstract;
+
+public interface IEntityRepository<T> where T : class, IEntity,new()
+{
+    Task<T> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+    Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null,
+        params Expression<Func<T, object>>[] includeProperties);
+    Task AddAsync(T entity);
+    Task Delete(T entity);
+    Task UpdateAsync(T entity);
+    Task<bool> Any(Expression<Func<T, bool>> predicate);
+    Task<int> Count(Expression<Func<T, bool>> predicate);
+}
---
+++ ExampleProject.Core/Data/Concrete/EntityFrameWork/EFEntityRepositoryBase.cs
@@ -0,0 +1,52 @@
+﻿using ExampleProject.Core.Data.Abstract;
+using ExampleProject.Core.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ExampleProject.Core.Data.Concrete.EntityFrameWork;
+
+public class EFEntityRepositoryBase<T> : IEntityRepository<T> where T : class, IEntity, new()
+{
+
+    private readonly DbContext _context;
+
+    public EFEntityRepositoryBase(DbContext context)
+    {
+        _context = context;
+    }
+
+    public async  Task AddAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> Any(Expression<Func<T, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<int> Count(Expression<Func<T, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task Delete(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<T> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
