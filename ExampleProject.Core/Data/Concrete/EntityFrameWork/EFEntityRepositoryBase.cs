8befb33efae7781986df097b1bc84d35f3a32ae0
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2023-05-03 22:02:30 +0300

    Genericrepository concrete created.

--- ExampleProject.Core/Data/Concrete/EntityFrameWork/EFEntityRepositoryBase.cs
+++ ExampleProject.Core/Data/Concrete/EntityFrameWork/EFEntityRepositoryBase.cs
@@ -15,38 +15,72 @@ public class EFEntityRepositoryBase<T> : IEntityRepository<T> where T : class, I
         _context = context;
     }
 
-    public async  Task AddAsync(T entity)
+    public async Task AddAsync(T entity)
     {
-        throw new NotImplementedException();
+        _context.Set<T>().Add(entity);
     }
 
     public async Task<bool> Any(Expression<Func<T, bool>> predicate)
     {
-        throw new NotImplementedException();
+        return await _context.Set<T>().AnyAsync(predicate);
     }
 
     public async Task<int> Count(Expression<Func<T, bool>> predicate)
     {
-        throw new NotImplementedException();
+        return await _context.Set<T>().CountAsync(predicate);
     }
 
     public async Task Delete(T entity)
     {
-        throw new NotImplementedException();
+        await Task.Run(() => (_context.Set<T>().Remove(entity)));
     }
 
     public async Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties)
     {
-        throw new NotImplementedException();
+        IQueryable<T> query = _context.Set<T>();
+        if (predicate != null)
+        {
+            query = query.Where(predicate);
+
+        }
+
+        if (includeProperties.Any())
+        {
+            foreach (var includeProperty in includeProperties)
+            {
+                query = query.Include(includeProperty);
+            }
+        }
+
+        return await query.ToListAsync();
     }
 
     public async Task<T> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties)
     {
-        throw new NotImplementedException();
+        IQueryable<T> query = _context.Set<T>();
+        if (predicate != null)
+        {
+            query = query.Where(predicate);
+
+        }
+
+        if (includeProperties.Any())
+        {
+            foreach (var includeProperty in includeProperties)
+            {
+                query = query.Include(includeProperty);
+            }
+        }
+
+        return await query.SingleOrDefaultAsync();
     }
 
     public async Task UpdateAsync(T entity)
     {
-        throw new NotImplementedException();
+        await Task.Run(() =>
+        {
+            _context.Set<T>().Update(entity);
+
+        });
     }
 }
